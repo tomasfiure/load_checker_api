e739afcc8ce77c0bb1483b7700d1745ee1fd927e
tomasfiure <123346779+tomasfiure@users.noreply.github.com>
2024-11-09 23:07:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ def hello():
     return jsonify({"message": "Hello, world!"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
